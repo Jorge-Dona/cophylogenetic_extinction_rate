dfd2d5a993e346aff8cbf335a879199a389c3963
Jorge Doña <Jorge-Dona@users.noreply.github.com>
2019-09-16 12:05:25 +0200

    Add files via upload

---
+++ appendix1.R
@@ -0,0 +1,24 @@
+# This function computes the “extinction rate from cophylogenies” (Ec), a statistics that uses data from event-based cophylogenetic analyses and might be informative to assess symbiont extinction risks.
+# See Doña & Johnson 2019 for more details. 
+
+require(DescTools)
+
+#Load the function
+ec <- function(L,E,S) {
+  
+  est=BinomCI(L, (E+S),
+              conf.level = 0.95,
+              method = "modified wilson")
+  paste("Ec=", est[1],";","Lower CI=", est[2],";", "Upper CI=", est[3])
+}
+
+#L = number of losses
+#E = Total number of events resulting from the cophylogenetic reconstruction (i.e., Losses+switches+duplications, etc.)
+#S = Number of host-switches
+
+# Example of usage:
+# L = 1; E = 10; S = 2
+# 1. Load the function (note that modified wilson is used as the default method for computing the CI; modify the function if you want to use a different method).
+# 2. For the number of events of this example, call it simply as follows: ec(1,10,2).  
+
+
